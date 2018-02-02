b76ea88a2ba92f725b52aa581c9199101e2e5bcb
gudrun.vene <gudrun.vene@khk.ee>
2018-02-02 09:15:58 +0200

    anmebaasi töötluse klass

--- conf.php
+++ conf.php
@@ -24,6 +24,7 @@ require_once LIB_DIR.'utils.php';
 require_once MODEL_DIR.'template.php'; // html vaade failide töötlus
 require_once MODEL_DIR.'http.php'; // HTTP töötlus klass
 require_once MODEL_DIR.'linkobject.php'; // Lingi töötluse klass
+require_once MODEL_DIR.'mysql.php'; // db töötluse klass
 
 // loome vajalikud objektid, mis on pidevalt tööl
 $http = new linkobject();
