b71e5414a828b8c2970a0b6343f54876ad7098ce
gudrun.vene <gudrun.vene@khk.ee>
2018-02-02 09:13:21 +0200

    paika pandud andmebaasiga ühendus.. kirjelduse algus

---
+++ model/mysql.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gudrun.vene
+ * Date: 2.02.2018
+ * Time: 9:00
+ */
+
+class mysql
+{
+    // klassi väljas
+    var $conn = false; // ühendus db serveriga
+    var $host = false; // db server
+    var $user = false; // db serveri kasutaja
+    var $pass = false; // db serveri kasutaja parool
+    var $dbname = false;
+
+    /**
+     * mysql constructor.
+     * @param bool $host
+     * @param bool $user
+     * @param bool $pass
+     * @param bool $dbname
+     */
+    public function __construct($host, $user, $pass, $dbname)
+    {
+        // määrame parameetrite kaudu kõik vajalikud väärtused
+        $this->host = $host;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->dbname = $dbname;
+        $this->connect(); // tekitame ühenduse andmebaasiga
+    } // db nimi, mis on kasutusel
+
+    // andmebaasiga ühenduse loomine
+    function connect($host, $user, $pass, $dbname){
+        $this->conn = mysqli_connect($this->host, $this->user, $this->pass, $this->dbname);
+        if(!$this->conn){
+            echo 'Probleem andmebaasi ühendusega<br />';
+            exit;
+            //siin ei tagasta mingit infot, sest conn sisu
+            // läheb ise dokumentatsiooni
+        }
+    }
+}
