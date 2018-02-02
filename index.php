e729113c0e4ed5604c0439ff2a51acaa130320fa
gudrun.vene <gudrun.vene@khk.ee>
2018-02-02 08:59:37 +0200

    sise väljastamine kontrollitud

--- index.php
+++ index.php
@@ -25,5 +25,11 @@ require_once 'menu.php';
 // väljastame sisuga täidetud mall
 echo $mainTmpl->parse();
 // kontrollime $http objekti tööd
-$control = $http->get('control');
-echo $control.'<br />';
+//$control = $http->get('control');
+//echo $control.'<br />';
+
+echo '<pre>';
+print_r($http->vars);
+echo '</pre>';
+
+
