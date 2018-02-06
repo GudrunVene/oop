858f917b484eadbb318a7dde39426456311a38b9
gudrun.vene <gudrun.vene@khk.ee>
2018-02-06 12:18:15 +0200

    menüü maha võetud

--- menu.php
+++ menu.php
@@ -10,41 +10,6 @@
 $menuTmpl = new template('menu.menu'); // menüü mall
 $itemTmpl = new template('menu.item'); // menüü elemendi mall
 
-// loome üks menüü element nimega esimene
-// määrame menüüs väljastava elemendi nime
-$itemTmpl->set('name', 'avaleht');
-// määrata menüüs väljastava elemendiga seotud link
-// http://anna.ikt.khk.ee/oop_vs17_2/index.php?control=esimene
-$link = $http->getLink();
-$itemTmpl->set('link', $link);
-// lisame antud element menüüsse
-$menuItem = $itemTmpl->parse(); // string, mis sisaldab ühe nimekirja elemendi lingiga
-$menuTmpl->add('menu_items', $menuItem); // nüüd
-
-
-// loome üks menüü element nimega esimene
-// määrame menüüs väljastava elemendi nime
-$itemTmpl->set('name', 'esimene');
-// määrata menüüs väljastava elemendiga seotud link
-// http://anna.ikt.khk.ee/oop_vs17_2/index.php?control=esimene
-$link = $http->getLink(array('control'=>'esimene'));
-$itemTmpl->set('link', $link);
-// lisame antud element menüüsse
-$menuItem = $itemTmpl->parse(); // string, mis sisaldab ühe nimekirja elemendi lingiga
-$menuTmpl->add('menu_items', $menuItem); // nüüd olemas paar 'menu_items'=>'<li>...</li>
-
-// loome veel üks menüü element nimega teine
-$itemTmpl->set('name', 'teine');
-// määrata menüüs väljastava elemendiga seotud link
-// http://anna.ikt.khk.ee/oop_vs17_2/index.php?control=esimene
-$link = $http->getLink(array('control'=>'teine'));
-$itemTmpl->set('link', $link);
-// lisame antud element menüüsse
-$menuItem = $itemTmpl->parse(); // string, mis sisaldab ühe nimekirja elemendi lingiga
-$menuTmpl->add('menu_items', $menuItem); // nüüd olemas paar 'menu_items'=>'<li>...</li>
-
-// ehitame valmis menüü
-$menu = $menuTmpl->parse();
-
-// lisame valmis menüü lehele
-$mainTmpl->set('menu', $menu);
+// koosta menüü ja sisu loomise päringut
+$sql = 'SELECT content_id, content, title'.'FROM content WHERE parent_id='.fixDB'AND show_in_menu='.fixDB;
+$result = $db->getData($sql); // loeme andmed anmebaasist
